597d671d529e84ed0a082f54ab7e9eed4f81465d
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-24 19:33:12 +0700

    feat: Подключить БД

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { PORT } = require('./constants');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.listen(PORT, () => {
   console.log('Сервер запущен на порту:', PORT);
