68d30b0b0094a02d8b08859bf58d6cf78cda698c
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-25 12:45:22 +0700

    feat: Добавить переменные с регулярными выражениями

--- constants.js
+++ constants.js
@@ -2,6 +2,11 @@ const {
   PORT = 3000,
 } = process.env;
 
+const REGEX_RU = /^[а-яё\s\d.!?-]+$/iu;
+const REGEX_EN = /^[a-z\s\d.!?-]+$/iu;
+
 module.exports = {
   PORT,
+  REGEX_RU,
+  REGEX_EN,
 };
