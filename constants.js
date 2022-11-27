4aa325923bc075bef8214fcef290fb0466b17728
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-27 14:16:57 +0700

    feat: Добавить переменную

--- constants.js
+++ constants.js
@@ -4,9 +4,11 @@ const {
 
 const REGEX_RU = /^[а-яё\s\d.!?-]+$/iu;
 const REGEX_EN = /^[a-z\s\d.!?-]+$/iu;
+const REGEX_URL = /^https?:\/\/(www\.)?[\w\d-]+\.\w/;
 
 module.exports = {
   PORT,
   REGEX_RU,
   REGEX_EN,
+  REGEX_URL,
 };
