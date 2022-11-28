c70a56b65be3fcd906a087c16868ce93e0b35e21
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-28 10:33:08 +0700

    feat: Создать роут для фильмов

---
+++ routes/movies.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { createFilm, getFilms, deleteFilm } = require('../controllers/movies');
+const { REGEX_URL, REGEX_EN, REGEX_RU } = require('../constants');
+
+router.get('/movies', getFilms); // возвращает все сохранённые текущим  пользователем фильмы
+router.post('/movies', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(REGEX_URL),
+    trailerLink: Joi.string().required().pattern(REGEX_URL),
+    thumbnail: Joi.string().required().pattern(REGEX_URL),
+    movieId: Joi.string().length(24).hex().required(),
+    nameRU: Joi.string().required().pattern(REGEX_RU),
+    nameEN: Joi.string().required().pattern(REGEX_EN),
+  }),
+}), createFilm); // создаёт фильм с переданными в теле
+router.delete('/movies/:id', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), deleteFilm); // удаляет сохранённый фильм по id
+
+module.exports = router;
