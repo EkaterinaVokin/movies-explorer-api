885313e5e8d09a922727e7eb3db0dceacceb9f51
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-28 15:48:43 +0700

    feat: Добавить централизованную обработку ошибок

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet'); // модуль для защиты приложения известных веб-уязвимостей
+const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const { PORT } = require('./constants');
+const NotFoundError = require('./errors/not-found-err');
+const routes = require('./routes/index');
+const auth = require('./middlewares/auth');
+const centerErrors = require('./middlewares/centerErrors');
 
 const app = express();
 
+app.use(express.json());
+
+app.use(helmet()); // безопасность
+
+app.use(cookieParser()); // подключаем парсер кук как мидлвэр
+
+// регистрация и авторизация
+app.use(routes);
+
+app.use('/', auth, require('./routes/users'));
+app.use('/', auth, require('./routes/movies'));
+
+// обработка несуществующих маршрутов
+app.use('*', auth, (req, res, next) => {
+  next(new NotFoundError(`Запрашиваемый ресурс ${req.baseUrl} не найден`));
+});
+
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
+// обработчики ошибок
 app.use(errors()); // обработчик ошибок celebrate
+
+app.use(centerErrors); // обработчки централизованных ошибок
 
 app.listen(PORT, () => {
   console.log('Сервер запущен на порту:', PORT);
