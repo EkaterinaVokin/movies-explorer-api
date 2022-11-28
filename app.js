672a80239a45d60b4af51ca45e669203d7ab0efa
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-28 10:35:23 +0700

    feat: Подключить к приложению обработчик ошибок celebrate

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 const { PORT } = require('./constants');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+
+app.use(errors()); // обработчик ошибок celebrate
 
 app.listen(PORT, () => {
   console.log('Сервер запущен на порту:', PORT);
