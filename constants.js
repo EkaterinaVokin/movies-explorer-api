2811acd8925b942e542d385d771109bbddbfe939
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-28 15:47:36 +0700

    feat: Добавить переменные окружения

--- constants.js
+++ constants.js
@@ -1,14 +1,20 @@
 const {
   PORT = 3000,
+  NODE_ENV,
+  JWT_SECRET,
 } = process.env;
 
 const REGEX_RU = /^[а-яё\s\d.!?-]+$/iu;
 const REGEX_EN = /^[a-z\s\d.!?-]+$/iu;
 const REGEX_URL = /^https?:\/\/(www\.)?[\w\d-]+\.\w/;
+const MONGO_CODE = 11000;
 
 module.exports = {
   PORT,
   REGEX_RU,
   REGEX_EN,
   REGEX_URL,
+  MONGO_CODE,
+  NODE_ENV,
+  JWT_SECRET,
 };
