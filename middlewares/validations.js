b8440d3efcf5b94be970d9fe64cad261871fc2e4
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-30 14:52:35 +0700

    fix: Вынести валидацию в отдельный файл

---
+++ middlewares/validations.js
@@ -0,0 +1,59 @@
+const { celebrate, Joi } = require('celebrate');
+const { REGEX_URL } = require('../utils/regex');
+
+// валидация авторизации
+const validationLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+// валидация регистрации
+const validationCreateUser = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+// валидация создание фильма
+const validationCreateFilm = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required().min(4).max(4),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(REGEX_URL),
+    trailerLink: Joi.string().required().pattern(REGEX_URL),
+    thumbnail: Joi.string().required().pattern(REGEX_URL),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+// валидация удалить фильм
+const validationDeleteFilm = celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().length(24).hex().required(),
+  }),
+});
+
+// валидация обновление профиля пользователя
+const validationUpdateProfile = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+  }),
+});
+
+module.exports = {
+  validationLogin,
+  validationCreateUser,
+  validationCreateFilm,
+  validationDeleteFilm,
+  validationUpdateProfile,
+};
