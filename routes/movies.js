0690fcac010e2810abedb684572c9036e6153b51
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-30 11:54:05 +0700

    fix: Отредактировать валидацию полей

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { createFilm, getFilms, deleteFilm } = require('../controllers/movies');
-const { REGEX_URL, REGEX_EN, REGEX_RU } = require('../constants');
+const { REGEX_URL } = require('../constants');
 
 router.get('/movies', getFilms); // возвращает все сохранённые текущим  пользователем фильмы
 router.post('/movies', celebrate({
@@ -9,14 +9,14 @@ router.post('/movies', celebrate({
     country: Joi.string().required(),
     director: Joi.string().required(),
     duration: Joi.number().required(),
-    year: Joi.string().required(),
+    year: Joi.string().required().min(4).max(4),
     description: Joi.string().required(),
     image: Joi.string().required().pattern(REGEX_URL),
     trailerLink: Joi.string().required().pattern(REGEX_URL),
     thumbnail: Joi.string().required().pattern(REGEX_URL),
-    movieId: Joi.string().length(24).hex().required(),
-    nameRU: Joi.string().required().pattern(REGEX_RU),
-    nameEN: Joi.string().required().pattern(REGEX_EN),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
   }),
 }), createFilm); // создаёт фильм с переданными в теле
 router.delete('/movies/:movieId', celebrate({
